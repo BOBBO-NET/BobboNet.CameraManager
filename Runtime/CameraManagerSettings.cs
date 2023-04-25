e469fe0ab3b8c8eb17cc0d359b297fe022c2d0c1
LAPTOP-3BPAUQ9G\Iseei <iseeicyhawkston@gmail.com>
2023-04-25 01:23:00 -0500

    Initial Commit

---
+++ Runtime/CameraManagerSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BobboNet.CameraAudioListener
+{
+    [System.Serializable]
+    public class CameraManagerSettings
+    {
+        public LayerMask mainCullingMask = ~0;
+        public LayerMask overlayCullingMask;
+    }
+}
